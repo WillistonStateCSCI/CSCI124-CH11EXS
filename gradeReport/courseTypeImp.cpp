563fc6babf5b467ec32415f98adadab44c7e3e73
Amanda Skinner <Amanda.Skinner.1@willistonstate.edu>
2025-10-15 19:24:52 +0000

    Left off adding print function to studentTypeImp

--- gradeReport/courseTypeImp.cpp
+++ gradeReport/courseTypeImp.cpp
@@ -16,3 +16,41 @@ using namespace std;
 
 //The definitions of the member functions of the class
 //courseType go here.
+
+void courseType::setCourseInfo(string cName, string cNo, int credits)
+{
+    courseName = cName;
+    courseNo = cNo;
+    courseCredits = credits;
+} //end setCourseInfo
+
+void courseType::print(ostream& outF)
+{
+    outF << left; //Step 1
+    outF << setw(8) << courseNo << " "; //Step 2
+    outF << setw(15) << courseName; //Step 3
+    outF << right; //Step 4
+    outF << setw(3) << courseCredits << " "; //Step 5
+} //end print
+
+courseType::courseType(string cName, string cNo, int credits)
+{
+    courseName = cName;
+    courseNo = cNo;
+    courseCredits = credits;
+} //end default constructor
+
+int courseType::getCredits()
+{
+    return courseCredits;
+} //end getCredits
+
+string courseType::getCourseNumber()
+{
+    return courseNo;
+}//end getCourseNumber
+
+string courseType::getCourseName()
+{
+    return courseName;
+} //end getCourseName
--- gradeReport/studentTypeImp.cpp
+++ gradeReport/studentTypeImp.cpp
@@ -18,3 +18,30 @@ using namespace std;
 
 //The definitions of the member functions of the class
 //studentType go here.
+
+void studentType::setInfo(string fName, string lName, int ID, int nOfCourses, bool isTPaid, courseType courses[], char cGrades[])
+{
+    int i;
+    setName(fName, lName); //set the name
+    sId = ID; //set the student ID
+    isTuitionPaid = isTPaid; //set isTuitionPaid
+    numberOfCourses = nOfCourses; //set the number of courses
+    //set the course information
+    for (i = 0; i < numberOfCourses; i++)
+    {
+    coursesEnrolled[i] = courses[i];
+    coursesGrade[i] = cGrades[i];
+    }
+    sortCourses(); //sort the array coursesEnrolled
+} //end setInfo
+
+studentType::studentType()
+{
+    numberOfCourses = 0;
+    sId = 0;
+    isTuitionPaid = false;
+    for (int i = 0; i < 6; i++)
+        coursesGrade[i] = '*';
+} //end default constructor
+
+//Need to add print function
